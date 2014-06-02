f1c988de6fe0ed59457df744e4734a5ce885a5a6
unknown <andpri@ANDPRI0212.Corp.Acxiom.net>
2014-06-02 17:30:35 -0500

    inital commit- handles server settings

---
+++ Settings.js
@@ -0,0 +1,26 @@
+﻿var Database = require('./Database.js');
+
+var settings = {};
+
+exports.initialize = function(callback) {
+    Database.getSettings(function(status, result) {
+        if(status !== "success")
+            console.log("Could not get settings. Database.getSettings returned status '" + status + "'.");
+        else {
+            for(var i = 0; i < result.length; i++) {
+                settings[result[i].name] = result[i].value;
+            }
+
+            callback();
+        }
+    });
+};
+
+exports.getSetting = function(name) {
+    return settings[name];
+};
+
+exports.setSettings = function(name, value) {
+    settings[name] = value;
+    Database.setSetting(name, value);
+};
